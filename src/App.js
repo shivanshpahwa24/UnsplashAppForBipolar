095401bfef094d75286ba80cd0e496a1b485cce6
shivansh_pahwa <shivanshpahwa24@gmail.com>
2020-09-21 21:51:40 +0530

    Deployment Ready

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-
-import Loader from "./components/Loader";
-import InfiniteScroll from "react-infinite-scroll-component";
 import ImageContainer from "./components/ImageContainer";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [images, setImage] = useState([]);
-
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
-  const fetchImages = async () => {
-    const apiRoot = "https://api.unsplash.com";
-    const accessKey = process.env.REACT_APP_ACCESSKEY;
-    try {
-      const response = await fetch(
-        `${apiRoot}/collections/139386/photos?client_id=${accessKey}`
-      );
-      const data = await response.json();
-
-      setImage([...images, ...data]);
-      console.log(data);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-
   return (
     <div className="page-container">
       <BrowserRouter>
-        {" "}
         <Navbar />
         <Header />
-        <InfiniteScroll
-          dataLength={images.length}
-          next={fetchImages}
-          hasMore={true}
-          loader={<Loader />}
-        >
-          <ImageContainer images={images} />
-        </InfiniteScroll>
+        <ImageContainer />
       </BrowserRouter>
     </div>
   );
--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,25 +1,45 @@
 import React, { useState, useEffect } from "react";
 import SingleImage from "./SingleImage";
 import { v4 as uuid } from "uuid";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "./Loader";
 
-const ImageContainer = ({ images }) => {
+const ImageContainer = () => {
+  const [image1, setImage1] = useState([]);
+  const [image2, setImage2] = useState([]);
+
+  const fetchImages = async (choice = "1580860") => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+
+    try {
+      const response = await fetch(
+        `${apiRoot}/collections/${choice}/photos?client_id=${accessKey}`
+      );
+      const data = await response.json();
+      if (choice === "1580860") {
+        setImage1([...image1, ...data]);
+      }
+      if (choice === "139386") {
+        setImage2([...image2, ...data]);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   const [option, setOption] = useState("1");
 
   const tabDecider = () => {
     if (option === "1") {
       document.getElementById("trending").style.color = "black";
       document.getElementById("new").style.color = " #838383";
-      document.getElementById("following").style.color = " #838383";
+      fetchImages("1580860");
     }
     if (option === "2") {
       document.getElementById("trending").style.color = " #838383";
       document.getElementById("new").style.color = "black";
-      document.getElementById("following").style.color = " #838383";
-    }
-    if (option === "3") {
-      document.getElementById("trending").style.color = " #838383";
-      document.getElementById("new").style.color = " #838383";
-      document.getElementById("following").style.color = "black";
+      fetchImages("139386");
     }
   };
   useEffect(() => {
@@ -49,22 +69,37 @@ const ImageContainer = ({ images }) => {
         >
           New
         </button>
-        <button
-          onClick={() => {
-            setOption("3");
-          }}
-          id="following"
-          className="btn nav-links"
-          style={{ fontSize: "1rem", fontWeight: "600" }}
+      </div>
+
+      {option === "1" && (
+        <InfiniteScroll
+          dataLength={image1.length}
+          next={() => fetchImages("1580860")}
+          hasMore={true}
+          loader={<Loader />}
         >
-          Following
-        </button>
-      </div>
-      <div className="image-container mx-2">
-        {images.map((image) => (
-          <SingleImage url={image.urls.small} key={uuid()} id={image.id} />
-        ))}
-      </div>
+          <div className="image-container mx-2">
+            {image1.map((image) => (
+              <SingleImage url={image.urls.small} key={uuid()} id={image.id} />
+            ))}
+          </div>
+        </InfiniteScroll>
+      )}
+
+      {option === "2" && (
+        <InfiniteScroll
+          dataLength={image2.length}
+          next={() => fetchImages("139386")}
+          hasMore={true}
+          loader={<Loader />}
+        >
+          <div className="image-container mx-2">
+            {image2.map((image) => (
+              <SingleImage url={image.urls.small} key={uuid()} id={image.id} />
+            ))}
+          </div>
+        </InfiniteScroll>
+      )}
     </div>
   );
 };
